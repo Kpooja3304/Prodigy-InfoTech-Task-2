8876a8ac80dfc5c462ad2e9fa60747aea0ede1a7
Kpooja3304 <139441759+Kpooja3304@users.noreply.github.com>
2024-06-30 01:16:37 -0700

    Add files via upload

---
+++ imageencdecoutput.py
@@ -0,0 +1,15 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: C:/Users/ADMIN/OneDrive/Desktop/Prodigy/imageencdec.py =======
+Enter path of Image : C:\Users\ADMIN\OneDrive\Pictures\xyz.jpg
+Enter Key for encryption of Image : 123
+The path of file :  C:\Users\ADMIN\OneDrive\Pictures\xyz.jpg
+Key for encryption :  123
+Encryption Done...
+Enter path of Image : C:\Users\ADMIN\OneDrive\Pictures\xyz.jpg
+Enter Key for encryption of Image : 123
+The path of file :  C:\Users\ADMIN\OneDrive\Pictures\xyz.jpg
+Note : Encryption key and Decryption key must be same.
+Key for Decryption :  123
+Decryption Done...
